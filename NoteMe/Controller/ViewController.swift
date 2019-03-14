6a3c8252f00e64a43f4b4b02b6844fcba27f23cb
Leonardo Domingues <leodmgs@gmail.com>
2019-03-13 22:18:30 -0300

    Create an instance of CoreDataBroker and recover the entity from the core data

--- NoteMe/Controller/ViewController.swift
+++ NoteMe/Controller/ViewController.swift
@@ -7,16 +7,39 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var coreDataBroker: CoreDataBroker = {
+        let coreData = CoreDataBroker(modelName: "NoteMe")
+        return coreData
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let entityDesc = NSEntityDescription.entity(forEntityName: "Note", in: self.coreDataBroker.managedObjectContext) {
+            print(entityDesc.name ?? "No name")
+            print(entityDesc.description)
+            let note = NSManagedObject(entity: entityDesc, insertInto: self.coreDataBroker.managedObjectContext)
+            note.setValue("This is the fucking first Note!", forKey: "title")
+            note.setValue(NSDate(), forKey: "createdAt")
+            note.setValue(NSDate(), forKey: "updatedAt")
+            print(note)
+            do {
+                try self.coreDataBroker.managedObjectContext.save()
+            } catch {
+                print("Unable to save managed object context")
+                print("\(error), \(error.localizedDescription)")
+            }
+        }
         setupView()
     }
 
     private func setupView() {
-        print("working well")
+        DispatchQueue.main.async {
+            self.view.backgroundColor = .white
+        }
     }
 
 }
