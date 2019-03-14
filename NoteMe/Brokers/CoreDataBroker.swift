2932e83aa6d0eed2cc578e6a72753ab6a25ea15e
Leonardo Domingues <leodmgs@gmail.com>
2019-03-14 17:16:24 -0300

    Add observer to listen notifications when the app will terminate and enter in background

--- NoteMe/Brokers/CoreDataBroker.swift
+++ NoteMe/Brokers/CoreDataBroker.swift
@@ -5,7 +5,7 @@
 //  Created by Leonardo Domingues on 3/7/19.
 //  Copyright © 2019 Leonardo Domingues. All rights reserved.
 //
-
+import UIKit
 import CoreData
 
 final /* it is not intended to be subclassed */
@@ -62,6 +62,23 @@ class CoreDataBroker {
     
     init(modelName: String) {
         self.modelName = modelName
+        setupNotificationsHandling()
+    }
+    
+    private func setupNotificationsHandling() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(saveChanges), name: UIApplication.willTerminateNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveChanges), name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+    
+    @objc private func saveChanges() {
+        guard managedObjectContext.hasChanges else { return }
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Unable to save changes with managed object context!")
+            print("\(error), \(error.localizedDescription)")
+        }
     }
     
 }
