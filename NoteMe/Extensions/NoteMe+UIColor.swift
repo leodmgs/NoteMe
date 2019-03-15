f1c029204da9550e0d7c51979eb75e3ff2bc4255
Leonardo Domingues <leodmgs@gmail.com>
2019-03-15 15:29:27 -0300

    Add an extension for UIColor to customize colors

---
+++ NoteMe/Extensions/NoteMe+UIColor.swift
@@ -0,0 +1,26 @@
+//
+//  NoteMe+UIColor.swift
+//  NoteMe
+//
+//  Created by Leonardo Domingues on 3/15/19.
+//  Copyright © 2019 Leonardo Domingues. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let noteWhite = UIColor(
+        red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0)
+    
+    static let noteGreen = UIColor(
+        red: 0.30, green: 0.85, blue: 0.39, alpha: 1)
+    
+    static let noteBlue = UIColor(
+        red: 0.0, green: 0.48, blue: 1, alpha: 1)
+    
+    static let noteRed = UIColor(
+        red: 1.0, green: 0.23, blue: 0.19, alpha: 1)
+    
+    static let noteTranslucent = UIColor(white: 1, alpha: 0.0)
+}
---
+++ NoteMe/View/NoteView.swift
@@ -0,0 +1,9 @@
+//
+//  NoteView.swift
+//  NoteMe
+//
+//  Created by Leonardo Domingues on 3/15/19.
+//  Copyright © 2019 Leonardo Domingues. All rights reserved.
+//
+
+import Foundation
